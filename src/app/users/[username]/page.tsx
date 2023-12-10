c0fe27038857619f2fa2633d2c169572169aacc3
Erik Hanchett <icystorm@gmail.com>
2023-12-10 13:51:59 -0800

    Added wild card route

---
+++ src/app/users/[username]/page.tsx
@@ -0,0 +1,14 @@
+import { headers } from "next/headers";
+import Links from "./Links";
+
+export default async function Users() {
+  const headersList = headers();
+
+  const hostname = headersList.get("host"); // to get domain
+
+  return (
+    <>
+      <h1>Hello {hostname}</h1>
+    </>
+  );
+}
